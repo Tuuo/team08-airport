09c78e40eb301a5d085f7977e5372ebf9c56e6cf
xq113 <lsfxwxq@163.com>
2023-06-12 17:19:33 +0800

    xml解析模块

---
+++ commit-Listener/src/main/java/com/isoft/CommitListenerApplication.java
@@ -0,0 +1,13 @@
+package com.isoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitListenerApplication.class, args);
+    }
+
+}
