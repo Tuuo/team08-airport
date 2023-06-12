1f77bb03e4ee770b8d2a39796f088ac56288b597
lixichen <9713259@qq.com>
2023-06-12 16:37:00 +0800

    实体类

--- Login/src/main/java/com/isoft/entity/Authority.java
+++ Login/src/main/java/com/isoft/entity/Authority.java
@@ -2,6 +2,8 @@ package com.isoft.entity;
 
 
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,7 +11,7 @@ import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity(name = "t_authority")
-
+@Data
 public class Authority  implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,36 +26,4 @@ public class Authority  implements Serializable {
         this.authority = authority;
     }
 
-    /**
-     * 获取
-     * @return id
-     */
-    public Integer getId() {
-        return id;
-    }
-
-    /**
-     * 设置
-     * @param id
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
-     * 获取
-     * @return authority
-     */
-    public String getAuthority() {
-        return authority;
-    }
-
-    /**
-     * 设置
-     * @param authority
-     */
-
-    public String toString() {
-        return "Authority{id = " + id + ", authority = " + authority + "}";
-    }
 }
--- Login/src/main/java/com/isoft/entity/Customer.java
+++ Login/src/main/java/com/isoft/entity/Customer.java
@@ -2,6 +2,7 @@ package com.isoft.entity;
 
 
 
+import lombok.Data;
 import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,7 +13,7 @@ import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity(name = "t_customer")
-
+@Data
 public class Customer implements Serializable {
     private static final long serialVersionUID = 7898396078272083417L;
     @Id
@@ -37,71 +38,5 @@ public class Customer implements Serializable {
         this.password = password;
     }
 
-    /**
-     * 获取
-     * @return id
-     */
-    public Integer getId() {
-        return id;
-    }
 
-    /**
-     * 设置
-     * @param id
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
-     * 获取
-     * @return username
-     */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * 设置
-     * @param username
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    /**
-     * 获取
-     * @return password
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * 设置
-     * @param password
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * 获取
-     * @return valid
-     */
-    public Integer getValid() {
-        return valid;
-    }
-
-    /**
-     * 设置
-     * @param valid
-     */
-    public void setValid(Integer valid) {
-        this.valid = valid;
-    }
-
-    public String toString() {
-        return "Customer{id = " + id + ", username = " + username + ", password = " + password + ", valid = " + valid + "}";
-    }
 }
