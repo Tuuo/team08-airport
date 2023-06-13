fb72ec904d8431fad63c3a54991d1ccb5e458b0c
李希晨 <971325919@qq.com>
2023-06-13 17:24:07 +0800

    补充的表 实体类

---
+++ commit-Listener/src/main/java/com/isoft/entity/Dflt_Dfdl_Arpt.java
@@ -0,0 +1,26 @@
+package com.isoft.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "DFOE_DFDL_ARPT")
+@Data
+public class Dflt_Dfdl_Arpt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String apno;
+    private String apcd;
+    private String fptt;
+    private String fett;
+    private String frtt;
+    private String fplt;
+    private String felt;
+    private String frlt;
+    private String apat;
+    private Long dflt_id;
+}
