a2cb8f797b71414afb72aca786c7d422e7f32e91
赵淑祺0515 <2949735580@qq.com>
2023-06-14 09:24:34 +0800

    数据库注释2

--- commit-Listener/src/main/resources/sql/李希晨.sql
+++ commit-Listener/src/main/resources/sql/李希晨.sql
@@ -142,7 +142,7 @@ CREATE TABLE TEAM08.DFOE_DFIE(
 	MSTA VARCHAR,--航班发布状态2，国内、国际、地区航班不使用该内容
 	MABS VARCHAR,--航班发布不正常状态2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际部分状态
 	MABR VARCHAR,--航班发布不正常原因2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际部分不正常原因
-	MIST VARCHAR,
+	MIST VARCHAR,--内部航班状态2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际部分状态
 	MIAS VARCHAR,--内部航班不正常状态2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际部分不正常原因
 	MIAR VARCHAR,--内部航班不正常状态2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际部分不正常原因
 	BORT VARCHAR,--航班开始登机实际时间，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内开始登机时间
@@ -172,28 +172,28 @@ CREATE TABLE TEAM08.DFOE_DFIE(
 	MCRS VARCHAR,--航班旅客值机实际开始时间2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际旅客值机时间
 	MCRE VARCHAR,--航班旅客值机实际结束时间2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际旅客值机时间
 	FCDP VARCHAR,--值机柜台及值机柜台区域显示1，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内旅客显示（按照集合的方式显示值机柜台）
-	MCDP VARCHAR,
-	CHLS VARCHAR,
-	STLS VARCHAR,
-	NMCD VARCHAR,
-	JMCD VARCHAR,
-	FLDT VARCHAR,
-	LLDT VARCHAR,
-	CONT VARCHAR,
-	PROX VARCHAR
+	MCDP VARCHAR,--值机柜台及值机柜台区域显示2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际旅客显示（按照集合的方式显示值机柜台）
+	CHLS VARCHAR,--航班行李滑槽动态更新信息标签
+	STLS VARCHAR,--航班机位动态更新信息标签
+	NMCD VARCHAR,--国内航站楼编号
+	JMCD VARCHAR,--国际航站楼编号
+	FLDT VARCHAR,--第一件行李上架时间
+	LLDT VARCHAR,--最后一件行李上架时间
+	CONT VARCHAR,--起降架次
+	PROX VARCHAR--航班代理
 );
 drop table TEAM08.DFOE_DFIE_ARPT;
 CREATE TABLE TEAM08.DFOE_DFIE_ARPT(
 	ID INT PRIMARY KEY IDENTITY(1,1),
-	APNO VARCHAR,
-	APCD VARCHAR,
-	FPTT VARCHAR,
-	FETT VARCHAR,
-	FRTT VARCHAR,
-	FPLT VARCHAR,
-	FELT VARCHAR,
-	FRLT VARCHAR,
-	APAT VARCHAR,
+	APNO VARCHAR,--航站序号
+	APCD VARCHAR,--航站三字码
+	FPTT VARCHAR,--计划起飞时间，如果有<DFLT>，如果航班为出港航班，第一航站（始发站）计划起飞时间必须有值
+	FETT VARCHAR,--预计起飞时间，如果有<DFLT>
+	FRTT VARCHAR,--实际起飞时间，如果有<DFLT>
+	FPLT VARCHAR,--计划降落时间，如果有<DFLT>
+	FELT VARCHAR,--预计降落时间，如果有<DFLT>
+	FRLT VARCHAR,--实际降落时间，如果有<DFLT>
+	APAT VARCHAR,--航展属性，（2403，2401，2404，2402），2403国内，2401国际，2404混合，2402地区
 	DFLT_ID NUMBER(6) REFERENCES TEAM08.DFOE_DFIE(FLID)
 );
 
@@ -201,85 +201,85 @@ drop table TEAM08.DFOE_DFDL;
 CREATE TABLE TEAM08.DFOE_DFDL(
 	ID INT IDENTITY(1,1),
 	META_ID	NUMBER(6) REFERENCES TEAM08.COMMON_META(SEQN),--关联META表
-	FLID NUMBER(6) PRIMARY KEY,
-	AFID VARCHAR,
-	FFID VARCHAR,
-	FIDE VARCHAR,
+	FLID NUMBER(6) PRIMARY KEY,--航班唯一编号
+	AFID VARCHAR,--关联航班ID
+	FFID VARCHAR,--航班标识
+	FIDE VARCHAR,--航班标识
 	TEST VARCHAR,
-	AWCD VARCHAR,
-	FLNO VARCHAR,
-	FEXD VARCHAR,
-	FIMD VARCHAR,
-	FLIO VARCHAR,
-	FLTK VARCHAR,
-	FATT VARCHAR,
-	PATT VARCHAR,
-	MFID VARCHAR,
-	MFFI VARCHAR,
-	CFTP VARCHAR,
-	CFNO VARCHAR,
-	STAT VARCHAR,
-	ABST VARCHAR,
-	ABRS VARCHAR,
-	ISTA VARCHAR,
-	IAST VARCHAR,
-	IARS VARCHAR,
-	MSTA VARCHAR,
-	MABS VARCHAR,
-	MABR VARCHAR,
-	MIST VARCHAR,
-	MIAS VARCHAR,
-	MIAR VARCHAR,
-	BORT VARCHAR,
-	MBOR VARCHAR,
-	TBRT VARCHAR,
-	MTBR VARCHAR,
-	LBDT VARCHAR,
-	MLBD VARCHAR,
-	POKT VARCHAR,
-	MPOK VARCHAR,
-	APOT VARCHAR,
-	DETT VARCHAR,
-	DRTT VARCHAR,
-	DELT VARCHAR,
-	DRLT VARCHAR,
-	VIP VARCHAR,
-	SFAW VARCHAR,
-	SFNO VARCHAR,
-	PAST VARCHAR,
-	GTLS VARCHAR,
-	BLLS VARCHAR,
-	FCES VARCHAR,
-	FCEE VARCHAR,
-	FCRS VARCHAR,
-	FCRE VARCHAR,
-	MCES VARCHAR,
-	MCEE VARCHAR,
-	MCRS VARCHAR,
-	MCRE VARCHAR,
-	FCDP VARCHAR,
-	MCDP VARCHAR,
-	CHLS VARCHAR,
-	STLS VARCHAR,
-	NMCD VARCHAR,
-	JMCD VARCHAR,
-	FLDT VARCHAR,
-	LLDT VARCHAR,
-	CONT VARCHAR,
-	PROX VARCHAR
+	AWCD VARCHAR,--航空公司二字码
+	FLNO VARCHAR,--航班号
+	FEXD VARCHAR,--航班执行日期，进港航班的计划降落时间里面的日期，出港航班的计划起飞时间里面的日期
+	FIMD VARCHAR,--航班所属计划批次的标识日期
+	FLIO VARCHAR,--航班进出标志，A-进港；D-出港
+	FLTK VARCHAR,--航班任务
+	FATT VARCHAR,--航班属性，（2403，2401，2404，2402），2403国内，2401国际，2404混合，2402地区
+	PATT VARCHAR,--航班属性旧值，（2403，2401，2404，2402），2403国内，2401国际，2404混合，2402地区
+	MFID VARCHAR,--主航班标识，虚拟航班主航班和独立航班此值为空，虚拟附属航班有值，代表此虚拟航班所挂靠的执行航班。
+	MFFI VARCHAR,--主航班标识，如果有<DFLT>
+	CFTP VARCHAR,--机型，国际民用航空组织指定代码ICAO
+	CFNO VARCHAR,--机号
+	STAT VARCHAR,--航班发布状态，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内部分状态
+	ABST VARCHAR,--航班发布不正常状态，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内部分状态
+	ABRS VARCHAR,--航班发布不正常原因，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内部分发布不正常原因
+	ISTA VARCHAR,--内部航班状态，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内部分状态
+	IAST VARCHAR,--内部航班不正常状态，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内部分状态
+	IARS VARCHAR,--内部航班不正常原因，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内部分不正常原因
+	MSTA VARCHAR,--航班发布状态2，国内、国际、地区航班不使用该内容
+	MABS VARCHAR,--航班发布不正常状态2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际部分状态
+	MABR VARCHAR,--航班发布不正常原因2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际部分不正常原因
+	MIST VARCHAR,--内部航班状态2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际部分状态
+	MIAS VARCHAR,--内部航班不正常状态2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际部分不正常原因
+	MIAR VARCHAR,--内部航班不正常状态2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际部分不正常原因
+	BORT VARCHAR,--航班开始登机实际时间，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内开始登机时间
+	MBOR VARCHAR,--航班开始登机实际时间，国内、国际、地区时不使用该内容，当航班属性为混合时特指国际开始登机时间
+	TBRT VARCHAR,--航班过站登机，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内过站机时间
+	MTBR VARCHAR,--航班过站登机时间，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际过站登机时间
+	LBDT VARCHAR,--航班催促登机实际时间，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内催促登机时间
+	MLBD VARCHAR,--航班催促登机实际时间，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际催促登机时间
+	POKT VARCHAR,--航班结束登机实际时间，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内结束登机时间
+	MPOK VARCHAR,--航班结束登机实际时间，国内、国际、地区不使用该内容，当航班属性为混合时特指国际结束登机时间
+	APOT VARCHAR,--备降航站
+	DETT VARCHAR,--备降预计起飞时间，如果有<APOT>
+	DRTT VARCHAR,--备降实际起飞时间，如果有<APOT>
+	DELT VARCHAR,--备降预计降落时间，如果有<APOT>
+	DRLT VARCHAR,--备降实际降落时间，如果有<APOT>
+	VIP VARCHAR,--VIP人数，如果该标签值为0，表示无VIP，大于0则表示有VIP，由于机场业务操作原因，可能导致该标签表示VIP人数不准确
+	SFAW VARCHAR,--共享航空二字码，如果有<SFLT>
+	SFNO VARCHAR,--共享航班号，如果有<SFLT>
+	PAST VARCHAR,--前站实际起飞时间
+	GTLS VARCHAR,--航班登机门动态信息标签
+	BLLS VARCHAR,--航班行李提取转盘动态信息标签
+	FCES VARCHAR,--航班旅客值机预计开始时间1，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内旅客值机时间
+	FCEE VARCHAR,--航班旅客值机预计结束时间1，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内旅客值机时间
+	FCRS VARCHAR,--航班旅客值机实际开始时间1，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内旅客值机时间
+	FCRE VARCHAR,--航班旅客值机实际结束时间1，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内旅客值机时间
+	MCES VARCHAR,--航班旅客值机预计开始时间2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际旅客值机时间
+	MCEE VARCHAR,--航班旅客值机预计结束时间2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际旅客值机时间
+	MCRS VARCHAR,--航班旅客值机实际开始时间2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际旅客值机时间
+	MCRE VARCHAR,--航班旅客值机实际结束时间2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际旅客值机时间
+	FCDP VARCHAR,--值机柜台及值机柜台区域显示1，当航班属性为国内、国际、地区时使用，当航班属性为混合时特指国内旅客显示（按照集合的方式显示值机柜台）
+	MCDP VARCHAR,--值机柜台及值机柜台区域显示2，国内、国际、地区航班不使用该内容，当航班属性为混合时特指国际旅客显示（按照集合的方式显示值机柜台）
+	CHLS VARCHAR,--航班行李滑槽动态更新信息标签
+	STLS VARCHAR,--航班机位动态更新信息标签
+	NMCD VARCHAR,--国内航站楼编号
+	JMCD VARCHAR,--国际航站楼编号
+	FLDT VARCHAR,--第一件行李上架时间
+	LLDT VARCHAR,--最后一件行李上架时间
+	CONT VARCHAR,--起降架次
+	PROX VARCHAR--航班代理
 );
 
 drop table TEAM08.DFOE_DFDL_ARPT;
 CREATE TABLE TEAM08.DFOE_DFDL_ARPT(
 	ID INT PRIMARY KEY IDENTITY(1,1),
-	APNO VARCHAR,
-	APCD VARCHAR,
-	FPTT VARCHAR,
-	FETT VARCHAR,
-	FRTT VARCHAR,
-	FPLT VARCHAR,
-	FELT VARCHAR,
-	FRLT VARCHAR,
-	APAT VARCHAR,
+	APNO VARCHAR,--航站序号
+	APCD VARCHAR,--航站三字码
+	FPTT VARCHAR,--计划起飞时间，如果有<DFLT>，如果航班为出港航班，第一航站（始发站）计划起飞时间必须有值
+	FETT VARCHAR,--预计起飞时间，如果有<DFLT>
+	FRTT VARCHAR,--实际起飞时间，如果有<DFLT>
+	FPLT VARCHAR,--计划降落时间，如果有<DFLT>
+	FELT VARCHAR,--预计降落时间，如果有<DFLT>
+	FRLT VARCHAR,--实际降落时间，如果有<DFLT>
+	APAT VARCHAR,--航站属性，（2403，2401，2404，2402），2403国内，2401国际，2404混合，2402地区
 	DFLT_ID NUMBER(6) REFERENCES TEAM08.DFOE_DFDL(FLID)
 );
