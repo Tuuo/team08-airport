0427c209aac66a701e3aee86dda2dfe15aa5c1b3
李希晨 <971325919@qq.com>
2023-06-15 09:22:05 +0800

    StringUtil和XmlAnaly的代码注释

--- commit-Listener/src/main/java/com/isoft/config/StringUtil.java
+++ commit-Listener/src/main/java/com/isoft/config/StringUtil.java
@@ -1,5 +1,9 @@
 package com.isoft.config;
 
+/*
+* 将传入的字符串去除首尾空格，并在去除空格后，如果结果是空字符串，则返回 null。
+* 这样可以方便地处理字符串的空值情况
+*/
 public class StringUtil {
     public static String trimToNull(String str) {
         if (str == null) {
