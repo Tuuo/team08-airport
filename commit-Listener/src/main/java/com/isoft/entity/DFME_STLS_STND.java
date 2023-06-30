558de5f76aa0a9b28b6831ab8fff4d10ee39b666
赵淑祺0515 <2949735580@qq.com>
2023-06-30 14:31:29 +0800

    注释

--- commit-Listener/src/main/java/com/isoft/entity/DFME_STLS_STND.java
+++ commit-Listener/src/main/java/com/isoft/entity/DFME_STLS_STND.java
@@ -16,18 +16,18 @@ public class DFME_STLS_STND {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Long meta_id;
-    private Long flid;
-    private String ffid;
-    private String fide;
-    private String fatt;
+    private Long flid;//航班标识标签
+    private String ffid;//航班标识
+    private String fide;//航班标识
+    private String fatt;//航班属性
     private Integer stls_id;
-    private String stno;
-    private String code;
-    private String estr;
-    private String eend;
-    private String rstr;
-    private String rend;
-    private String cssi;
-    private String btsc;
+    private String stno;//机位序号
+    private String code;//机位编号，如果有<STND>则M
+    private String estr;//预计开始使用时间，如果有<STND>则M
+    private String eend;//预计结束使用时间，如果有<STND>则M
+    private String rstr;//实际开始使用时间，如果有<STND>则M
+    private String rend;//实际结束时间，如果有<STND>则M
+    private String cssi;//当前停放机位b标示，如果有<STND>则M
+    private String btsc;//资源所属楼号，如果有<STND>则M
 
 }
