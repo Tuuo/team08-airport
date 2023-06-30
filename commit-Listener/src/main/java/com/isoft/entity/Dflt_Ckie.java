ee964abef07a3cb88966081eb6c71e7e34a5e7c7
赵淑祺0515 <2949735580@qq.com>
2023-06-30 14:51:32 +0800

    注

--- commit-Listener/src/main/java/com/isoft/entity/Dflt_Ckie.java
+++ commit-Listener/src/main/java/com/isoft/entity/Dflt_Ckie.java
@@ -14,14 +14,14 @@ public class Dflt_Ckie {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long meta_id;
-    private String flid;
-    private String ffid;
-    private String fide;
-    private String fatt;
-    private String stat;
-    private String ista;
-    private String fcrs;
-    private String msta;
-    private String mist;
-    private String mcrs;
+    private String flid;//航班标识标签
+    private String ffid;//航班标识
+    private String fide;//航班标识
+    private String fatt;//航班属性
+    private String stat;//航班发布状态
+    private String ista;//航班状态
+    private String fcrs;//航班旅客值机实际开始时间
+    private String msta;//航班发布状态2
+    private String mist;//内部航班状态2
+    private String mcrs;//航班旅客值机实际开始时间2
 }
--- commit-Listener/src/main/java/com/isoft/entity/Dflt_Ckls_Cntr.java
+++ commit-Listener/src/main/java/com/isoft/entity/Dflt_Ckls_Cntr.java
@@ -13,30 +13,30 @@ public class Dflt_Ckls_Cntr {
     @Id
     private Integer cntr_id;
     private Long meta_id;
-    private Long flid;
-    private String ffid;
-    private String fide;
-    private String fatt;
-    private String fces;
-    private String fcee;
-    private String fcrs;
-    private String fcre;
-    private String mces;
-    private String mcee;
-    private String mcrs;
-    private String mcre;
-    private String fcdp;
-    private String mcdp;
-    private String ckno;
-    private Long id;
-    private String code;
-    private String ckat;
-    private String type;
-    private String ccar;
-    private String estr;
-    private String eend;
-    private String rstr;
-    private String rend;
-    private String btsc;
+    private Long flid;//航班标识标签
+    private String ffid;//航班标识
+    private String fide;//航班标识
+    private String fatt;//航班属性
+    private String fces;//航班旅客值机预计开始时间1
+    private String fcee;//航班旅客值机预计结束时间1
+    private String fcrs;//航班旅客值机实际开始时间1
+    private String fcre;//航班旅客值机实际结束时间1
+    private String mces;//航班旅客值机预计开始时间2
+    private String mcee;//航班旅客值机预计结束时间2
+    private String mcrs;//航班旅客值机实际开始时间2
+    private String mcre;//航班旅客值机实际结束时间2
+    private String fcdp;//值机柜台及值机柜台区域显示1
+    private String mcdp;//值机柜台及值机柜台区域显示2
+    private String ckno;//值机柜台序号
+    private Long id;//值机柜台唯一标识
+    private String code;//值机柜台编号
+    private String ckat;//值机柜台属性
+    private String type;//柜台类型
+    private String ccar;//值机柜台所属值机岛号标签
+    private String estr;//预计开始使用时间
+    private String eend;//预计结束使用时间
+    private String rstr;//实际开始使用时间
+    private String rend;//实际结束使用时间
+    private String btsc;//资源所属楼号
     private Long dflt_id;
 }
