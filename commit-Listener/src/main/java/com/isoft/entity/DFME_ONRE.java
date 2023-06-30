93dc7db1790792f363aff4e84d8632649b383529
赵淑祺0515 <2949735580@qq.com>
2023-06-30 14:18:10 +0800

    1

--- commit-Listener/src/main/java/com/isoft/entity/DFME_ONRE.java
+++ commit-Listener/src/main/java/com/isoft/entity/DFME_ONRE.java
@@ -12,16 +12,16 @@ public class DFME_ONRE {
     @Id
     private Integer id;
     private Long meta_id;
-    private Long flid;
-    private String ffid;
-    private String fide;
-    private String fatt;
-    private String stat;
-    private String ista;
-    private String msta;
-    private String mist;
-    private String past;
-    private String eldt;
+    private Long flid; //航班标识标签
+    private String ffid;//航班标识
+    private String fide;//航班标识
+    private String fatt;//航班属性
+    private String stat;//航班发布状态
+    private String ista;//航班状态
+    private String msta;//航班发布状态2
+    private String mist;//内部航班状态2
+    private String past;//前站实际起飞时间
+    private String eldt;//本站预计降落时间
 
 
 }
